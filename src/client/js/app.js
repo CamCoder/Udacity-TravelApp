3b91c841753687785b811e0637d90114ecbe7ac3
CamCoder <camcam.95@icloud.com>
2021-06-25 22:28:51 -0500

    cleaned up code

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -11,11 +11,10 @@ const pixURL ="https://pixabay.com/api/?key=";
 const PIX_KEY = '22240298-415423148879f96b44082537c';
 
 // global var
-let country_code = '';
+// let country_code = '';
 
-/**
- * Add an item to a localStorage() array
-*/
+
+//  Add an item to a localStorage() array
 function addToLocalStorageArray(name, value) {
 
 	// Get the existing data
@@ -37,20 +36,12 @@ export function generateEntry(loc){
     // Get lon and lat
     getCord(loc)
     // Use the lon and lat to get weather and pic
-    .then(async function(data){
-
-        
+    .then(async function(data){   
         await getWeather(data.geonames[0].lat,data.geonames[0].lng);
-        
-        // if(data.geonames[0].countryCode == 'US'){
-        //     getImage(data.geonames[0].name, data.geonames[0].adminName1)
-        // }else{
-        //     getImage(data.geonames[0].name, data.geonames[0].countryName)
-        // }
         return data
     })
     .then( async function(data){
-        console.log("Second 2: ",data)
+
         if(data.geonames[0].countryCode == 'US'){
             await  getImage(data.geonames[0].name, data.geonames[0].adminName1)
         }else{
@@ -69,7 +60,6 @@ const getCord = async (location) => {
     const res = await fetch(geoURL + location + '&maxRows=1&username=' + GEO_KEY)
     try {
         const data = await res.json()
-        // console.log("TODAY: ", data.geonames[0].lat, data.geonames[0].lng)
 
         // Add city and state/country to local storage
         if(data.geonames[0].countryCode == "US"){
@@ -98,38 +88,10 @@ const getWeather = async (lat, lng) => {
 
             for(let x = 0; x<7;  x++){
                 addToLocalStorageArray(tripCount,[weather.data[x].weather.icon, weather.data[x].high_temp, weather.data[x].low_temp])
-                // console.log(tripCount,[weather.data[x].weather.icon, weather.data[x].high_temp, weather.data[x].low_temp])
-
             }
-
-            // console.log("Weather", weather.country_code)
-            country_code = weather.country_code;
-            console.log('First: ,', country_code)
-            // return weather;
         } catch (error) {
             console.log(error)
-        }
-
-
-
-
-    //     fetch('http://localhost:8082/weather',{
-    //         method: 'POST', 
-    //         credentials: 'same-origin',
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //         },      
-    //         body: JSON.stringify({lat,lng}), 
-    //     })     
-    //     .then(res => res.json())
-    //     .then(function(res){
-            
-    //         for(let x = 0; x<7;  x++){
-    //             addToLocalStorageArray(tripCount,[res.data[x].weather.icon, res.data[x].high_temp, res.data[x].low_temp])
-
-    //         }
-    //         tripList();
-    //     })        
+        }     
     }
     // else{
     //     fetch('http://localhost:8082/weatherHistory',{
@@ -151,18 +113,14 @@ const getWeather = async (lat, lng) => {
 
 }
 
-
+// Get image of location
 const getImage = async (param1,param2) =>{
 
-    const res = await fetch(pixURL+PIX_KEY+"&q="+ param1 + "+" + param2+ "&image_type=photo");
+    const res = await fetch(pixURL+PIX_KEY+"&q="+ param1 + "+" + param2+ "+city&image_type=photo");
     try {
         const image = await res.json()
         localStorage.setItem("tripCount",tripCount);
         addToLocalStorageArray(tripCount++,image.hits[0].webformatURL) ;
-        // console.log("LASTTT")
-        // console.log(localStorage.getItem(tripCount-1).split(','))
-        // tripCount++;
-        // return image.hits[0].webformatURL;
     } catch (error) {
         console.log(error)
     }
