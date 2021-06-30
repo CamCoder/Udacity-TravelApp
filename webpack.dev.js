74ce72f4ad5cd2cbc6ab94b0752ef7f56e8d44b7
CamCoder <camcam.95@icloud.com>
2021-06-29 21:06:19 -0500

    working on css

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const WorkboxPlugin = require('workbox-webpack-plugin');
+// const WorkboxPlugin = require('workbox-webpack-plugin');
 
 
 
@@ -43,7 +43,7 @@ module.exports = {
         }),
         // new WorkboxPlugin.GenerateSW(),
         new MiniCssExtractPlugin({ filename: "[name].css" }),
-        new WorkboxPlugin.GenerateSW(),
+        // new WorkboxPlugin.GenerateSW(),
         
     ],     
 }
